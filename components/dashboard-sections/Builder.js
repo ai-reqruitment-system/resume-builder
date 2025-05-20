fd81b40a6cc4aa97b0c4fa1d41382834539c4b4b
Shriram Tiwari <shriramt.124@gmail.com>
2025-05-20 20:01:01 +0530

    "testing branch change"

--- components/dashboard-sections/Builder.js
+++ components/dashboard-sections/Builder.js
@@ -3,6 +3,7 @@ import BuilderPage from '@/pages/builder.js';
 
 export default function Builder({ onClose }) {
     console.log('Builder component rendered');
+    console.log('onClose prop:', onClose);
     return (
         <div className="w-full mx-auto">
             <BuilderPage onClose={onClose} />
