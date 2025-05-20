c6aa067918698433bafe67ba116c3d79303af702
Shriram Tiwari <shriramt.124@gmail.com>
2025-05-20 19:40:55 +0530

    "git branch testing"

--- components/dashboard-sections/Builder.js
+++ components/dashboard-sections/Builder.js
@@ -2,6 +2,7 @@ import React from 'react';
 import BuilderPage from '@/pages/builder.js';
 
 export default function Builder({ onClose }) {
+    console.log('Builder component rendered');
     return (
         <div className="w-full mx-auto">
             <BuilderPage onClose={onClose} />
