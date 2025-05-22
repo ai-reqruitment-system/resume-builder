f165fb86f77d78565f250a50938529989aaf25f8
Shriram Tiwari <shriramt.124@gmail.com>
2025-05-23 01:53:41 +0530

    "ecosystem file "

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,18 @@
 module.exports = {
-  apps: [{
-    name: 'vuexy-nextjs-admin',
-    script: 'npm',
-    args: 'start',
-    env: {
-      PORT: 5010,
-      NODE_ENV: 'production'
-    }
-  }]
-}
+  apps: [
+    {
+      name: "Resuming.io",
+      script: "./node_modules/next/dist/bin/next", // Directly reference the JS file
+      args: "start",
+      watch: true,
+      env: {
+        PORT: 3000,
+        NODE_ENV: "development",
+      },
+      env_production: {
+        PORT: 80,
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
